0678b04214debe3ce9f55fab8eef2ea1b3a37ee3
kurtis21 <48065494+kurtis21@users.noreply.github.com>
2020-07-15 16:09:50 -0500

    this is my first update.  I added electron to my react app.

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Kurtis makes an app
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://wisconsin.edu"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         Go to an external link
         </a>
       </header>
     </div>

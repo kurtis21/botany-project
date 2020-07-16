42f264c4648811dd9fd817f8b4f28a4fd66e8c7a
kurtis21 <48065494+kurtis21@users.noreply.github.com>
2020-07-15 20:27:11 -0500

    Adding SQLite3
    for creating the database file

--- public/Main.js
+++ public/Main.js
@@ -1,10 +1,27 @@
-const {app, BrowserWindow} = require('electron')
+const sqlite3 = require('sqlite3').verbose();
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
 
 function createWindow () {
-    win = new BrowserWindow({width:800, height:600})
+    win = new BrowserWindow({width:800, height:600});
 
-    win.loadURL('http://localhost:3000/')
-}
+    win.loadURL('http://localhost:3000/');
+};
 
-app.on('ready', createWindow)
+const dbPath = path.resolve(__dirname, 'pollen.db');
 
+let db = new sqlite3.Database(dbPath, (err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Great success! VERY NICE!');
+});
+
+db.run("CREATE TABLE pollen(name text)");
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    db.close();
+    app.quit();
+});
--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log("this is where javascript code goes");
+
   return (
     <div className="App">
       <header className="App-header">
